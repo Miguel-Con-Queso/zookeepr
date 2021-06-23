ce99b7430d776ac1c7ee8100786a64a2d8536591
MiguelConQueso <michael_pnkstn@yahoo.com>
2021-06-23 19:57:57 -0400

    stopping for class

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const router = require('express').Router();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
